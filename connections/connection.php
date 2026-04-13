6e85425cd3c17f5a5f552e2b734377b05f8d12ae
buyson-nai <sana.ronaldian@ue.edu.ph>
2026-04-13 21:13:02 +0800

    fixed connection

--- connections/connection.php
+++ connections/connection.php
@@ -1,10 +1,24 @@
 <?php
+function envValue($key) {
+    return $_ENV[$key] ?? $_SERVER[$key] ?? getenv($key) ?? null;
+}
+
 function connection() {
-    $host = getenv('MYSQLHOST');
-    $user = getenv('MYSQLUSER');
-    $pass = getenv('MYSQLPASSWORD');
-    $dbname = getenv('MYSQLDATABASE');
-    $port = getenv('MYSQLPORT');
+    $host = envValue('MYSQLHOST');
+    $user = envValue('MYSQLUSER');
+    $pass = envValue('MYSQLPASSWORD');
+    $dbname = envValue('MYSQLDATABASE');
+    $port = envValue('MYSQLPORT');
+
+    if (!$host || !$user || !$dbname || !$port) {
+        die(
+            "Missing DB env vars.<br>" .
+            "MYSQLHOST: " . var_export($host, true) . "<br>" .
+            "MYSQLUSER: " . var_export($user, true) . "<br>" .
+            "MYSQLDATABASE: " . var_export($dbname, true) . "<br>" .
+            "MYSQLPORT: " . var_export($port, true)
+        );
+    }
 
     $conn = new mysqli($host, $user, $pass, $dbname, (int)$port);
 
@@ -14,4 +28,4 @@ function connection() {
 
     return $conn;
 }
-?> 
+?>
